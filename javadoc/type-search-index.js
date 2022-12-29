23405c5707461b9864b2596b5416c38b2f20cbd4
Katrin Valge <katrin.valge@mail.ee>
2022-12-29 23:02:48 +0200

    Added javadoc.

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.lib.bibliotheca","l":"BibliothecaApplication"},{"p":"com.lib.bibliotheca","l":"BibliothecaApplicationTests"}];updateSearchResults();

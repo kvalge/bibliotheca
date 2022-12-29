7cb786c2f8e7f3d8fdd620625a0921c438a44237
Katrin Valge <katrin.valge@mail.ee>
2022-12-29 22:51:41 +0200

    Initial commit.

---
+++ src/test/java/com/lib/bibliotheca/BibliothecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lib.bibliotheca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
